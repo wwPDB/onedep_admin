d328a73b7fa8b07f0ed1598f7b49c51d7ab83354
Ezra Peisach <ezra.peisach@rcsb.org>
2019-05-30 17:11:49 -0400

    Check annotation-pack version if present in config file

--- scripts/RunUpdate.py
+++ scripts/RunUpdate.py
@@ -3,9 +3,9 @@
 """Utility to help run/manage update of OneDep system"""
 
 try:
-    from ConfigParser import ConfigParser
+    from ConfigParser import ConfigParser, NoOptionError
 except ImportError:
-    from configparser import ConfigParser
+    from configparser import ConfigParser, NoOptionError
 try:
     from urllib.parse import urlparse
 except ImportError:
@@ -13,6 +13,7 @@ except ImportError:
 import subprocess
 import argparse
 import os.path
+import json
 
 from wwpdb.utils.config.ConfigInfo import ConfigInfo
 from wwpdb.utils.db.MyConnectionBase import MyConnectionBase
@@ -97,10 +98,10 @@ class UpdateManager(object):
         # print(self.__cparser.defaults())
         # print()
 
-    def __exec(self, cmd):
+    def __exec(self, cmd, overridenoop = False):
         print(cmd)
         ret = 0
-        if not self.__noop:
+        if not self.__noop or overridenoop:
             ret = subprocess.call(cmd, shell=True)
         return ret
 
@@ -124,23 +125,29 @@ class UpdateManager(object):
         command = 'cd {}; git pull; git checkout {}; git pull'.format(resdir, restag)
         self.__exec(command)
 
+    def checkwebfe(self, overridenoop = False):
+        webappsdir = self.__ci.get('TOP_WWPDB_WEBAPPS_DIR')
+        webdir = os.path.abspath(os.path.join(webappsdir, '..'))
+        curdir = os.path.dirname(__file__)
+        checkscript = os.path.join(curdir, 'ManageWebFE.py')
+        webfecheck = self.__cparser.get('DEFAULT', 'webfeconf')
+
+        command = 'python {} --webroot {} check -r {}'.format(checkscript, webdir, webfecheck)
+        ret = self.__exec(command, overridenoop = overridenoop)
+        if ret:
+            print("ERROR: check of webfe directory failed")
+        
+
     def updatewebfe(self):
         webappsdir = self.__ci.get('TOP_WWPDB_WEBAPPS_DIR')
         webdir = os.path.abspath(os.path.join(webappsdir, '..'))
         webfetag = self.__cparser.get('DEFAULT', 'webfetag')
-        webfecheck = self.__cparser.get('DEFAULT', 'webfeconf')
-        curdir = os.path.dirname(__file__)
-        checkscript = os.path.join(curdir, 'ManageWebFE.py')
 
         command = 'cd {}; git pull; git checkout {}; git pull ; git submodule init; git submodule update'.format(webdir, webfetag)
         self.__exec(command)
 
         # Now check the results
-
-        command = 'python {} --webroot {} check -r {}'.format(checkscript, webdir, webfecheck)
-        ret = self.__exec(command)
-        if ret:
-            print("ERROR: check of webfe directory failed")
+        self.checkwebfe()
 
     def updatetaxdb(self):
         # Checks the number of rows in db and decides if to update
@@ -181,6 +188,34 @@ class UpdateManager(object):
         dbs = DbSchemaManager(self.__noop)
         dbs.updateschema()
 
+    def checktoolvers(self):
+        #  vers_config_var,  configinfovar,             relative path
+        confs = [['annotver', 'SITE_ANNOT_TOOLS_PATH', 'etc/bundleversion.json'],
+                 ]
+
+        for c in confs:
+            varname = c[0]
+            confvar = c[1]
+            fpart = c[2]
+
+            try:
+                tvers = self.__cparser.get('DEFAULT', varname)
+                toolspath = self.__ci.get(c[1])
+                fname = os.path.join(toolspath, fpart)
+                if not os.path.exists(fname):
+                    print("WARNING: Tool out of date. %s not found" % fname)
+                    continue
+                with open(fname, 'r') as fin:
+                    jdata = json.load(fin)
+                    vstring = jdata['Version']
+                    if vstring != tvers:
+                        print("WARNING: Tool version mismatch %s != %s in %s" % (tvers, vstring, fname))
+            except NoOptionError as e:
+                # Option not in config file - continue
+                pass
+
+
+#        pass
 
 def main():
     parser = argparse.ArgumentParser()
@@ -191,6 +226,7 @@ def main():
     parser.add_argument("--skip-webfe", default=False, action='store_true', help='Skip webfe update')
     parser.add_argument("--skip-taxdb", default=False, action='store_true', help='Skip update of taxdb if needed')
     parser.add_argument("--skip-schema", default=False, action='store_true', help='Skip update of DB schemas if needed')
+    parser.add_argument("--skip-toolvers", default=False, action='store_true', help='Skip checking versions of tools')
 
     args = parser.parse_args()
     print(args)
@@ -217,6 +253,12 @@ def main():
     if not args.skip_schema:
         um.updateschema()
 
+    if not args.skip_toolvers:
+        um.checktoolvers()
+
+    # Final check on webfe
+    if not args.skip_webfe:
+        um.checkwebfe(overridenoop = True)
 
 if __name__ == '__main__':
     main()

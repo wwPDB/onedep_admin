9aba1a7697e54f9cb002ae655ad3f00f3fa2e9e1
jmb <jmb@ebi.ac.uk>
2020-01-27 10:38:52 +0000

    added documentation ensured that build version is correctly passed

--- scripts/BuildTools.py
+++ scripts/BuildTools.py
@@ -19,9 +19,10 @@ from wwpdb.utils.config.ConfigInfo import ConfigInfo
 
 
 class BuildTools(object):
-    def __init__(self, config_file, noop):
+    def __init__(self, config_file, noop, build_version='v-3300'):
         self.__configfile = config_file
         self.__noop = noop
+        self.__build_version = build_version
 
         # Infer topdir from where running from
         topdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -53,22 +54,13 @@ class BuildTools(object):
             print("About to build %s" % pbuild)
             self.run_build_command(pbuild=pbuild)
 
-    def run_build_command(self, pbuild, build_version='v-3300'):
+    def run_build_command(self, pbuild):
         onedep_build_dir = self.__ci.get('WWPDB_ONEDEP_BUILD')
-        onedep_build_dir_version = os.path.join(onedep_build_dir, build_version)
-        #distrib_dir = os.environ.get('DISTRIB_DIR', None)
+        onedep_build_dir_version = os.path.join(onedep_build_dir, self.__build_version)
 
         cmd = ['#!/bin/bash']
 
-        #if distrib_dir:
-        #    if os.path.exists(distrib_dir):
-        #        cmd.append('rm -rf ${DISTRIB_DIR}/*')
-        #    else:
-        #        os.makedirs(distrib_dir)
-        #else:
-        #    print('DISTRIB_DIR not defined - exiting')
-        #    sys.exit(1)
-
+        # set the environment and ensure we are up to date
         cmd.append('cd {}'.format(onedep_build_dir))
         cmd.append('git pull')
         cmd.append('. {}/utils/pkg-utils-v2.sh'.format(onedep_build_dir))
@@ -76,8 +68,20 @@ class BuildTools(object):
         cmd.append('export FORCE_REBUILD="YES"')
         cmd.append('. {}/packages/all-packages.sh'.format(onedep_build_dir_version))
 
+        # clear out the existing distrib dir so files are re-fetched
+        cmd.append('if [ -z "$DISTRIB_DIR"]')
+        cmd.append('then')
+        cmd.append('echo "DISTRIB_DIR not defined - exiting"')
+        cmd.append('exit 1')
+        cmd.append('else')
+        cmd.append('echo "remove files from distrib_dir so that they are rebuilt"')
+        cmd.append('rm -rf ${DISTRIB_DIR}/*')
+        cmd.append('fi')
+
+        # append the actual command
         cmd.append(pbuild)
 
+        # write everything to a temp file
         working_dir = tempfile.mkdtemp()
         temp_file = os.path.join(working_dir, 'cmd.sh')
         print('writing out commands to: {}'.format(temp_file))
@@ -85,7 +89,9 @@ class BuildTools(object):
         with open(temp_file, 'w') as outFile:
             outFile.write('\n'.join(cmd))
 
-        #return self.__exec(cmd_string)
+        # run the temp file
+        cmd_string = 'chmod +x {0}; {0}; rm -f {0}'.format(temp_file)
+        return self.__exec(cmd_string)
 
 
 
